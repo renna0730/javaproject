4e966570cab40f2713fe6a5ed2e98964b166b20e
renna0730 <renna0730@naver.com>
2025-04-07 09:48:57 +0900

    산술 연산자, Math.pow

---
+++ src/chapter03/Code3_06.java
@@ -0,0 +1,21 @@
+package chapter03;
+
+import java.util.Scanner;
+
+public class Code3_06 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter num1: ");
+        int num1 = s.nextInt();
+
+        System.out.print("Enter num2: ");
+        int num2 = s.nextInt();
+
+
+        double result = Math.pow(num1, num2);
+        System.out.printf("%d의 %d의 제곱승은 %.0f 입니다.", num1, num2, result);
+
+
+        s.close();
+    }
+}
---
+++ src/chapter03/Code3_Lab01.java
@@ -0,0 +1,25 @@
+package chapter03;
+
+import java.util.Scanner;
+
+public class Code3_Lab01 {
+    public static void main(String[] args) {
+        double lb = 0.543592;
+        double kg = 2.204623;
+
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter lb: ");
+        int num1 = s.nextInt();
+        System.out.print("Enter kg: ");
+        int num2 = s.nextInt();
+
+        double result1 = lb * num2;
+        double result2 = kg * num1;
+
+        System.out.printf("%d 파운드는 %f 킬로그램입니다.\n", num1, result2);
+        System.out.printf("%d 파운드는 %f 킬로그램입니다.", num2, result1);
+        s.close();
+
+
+    }
+}
