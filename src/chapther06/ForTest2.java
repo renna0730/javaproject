1141714750011382cf7d4dfc7263767cebc54caa
renna0730 <renna0730@naver.com>
2025-05-12 09:50:15 +0900

    6장 반복문, for2, 한 줄에 n개씩 출력하기

---
+++ src/chapther06/ForTest2.java
@@ -0,0 +1,11 @@
+package chapther06;
+
+public class ForTest2 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i + 1 + "\t");
+            if((i+1) % 10 == 0)
+                System.out.println();
+        }
+    }
+}
